c9640df68706e4cf60744f00411a42bcc1064ae4
josmunpen <josemamup@gmail.com>
2018-11-21 18:39:42 +0100

    21-11

--- Acme-Handy-Worker/src/main/java/repositories/CustomerRepository.java
+++ Acme-Handy-Worker/src/main/java/repositories/CustomerRepository.java
@@ -2,6 +2,7 @@
 package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Customer;
@@ -9,4 +10,11 @@ import domain.Customer;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
+	@Query("select c from Customer c where c.userAccount.id = ?1")
+	Customer findByUserAccountId(int userAccount);
+
+	//Query 11.1
+	//Navigate to customer from fixuptask
+	@Query("select c from Customer c join c.fixUpTasks f where f.id=?1")
+	Customer findFixUpTaskById(int fixUpTaskId);
 }
--- Acme-Handy-Worker/src/main/java/repositories/FixUpTaskRepository.java
+++ Acme-Handy-Worker/src/main/java/repositories/FixUpTaskRepository.java
@@ -12,7 +12,21 @@ import domain.FixUpTask;
 @Repository
 public interface FixUpTaskRepository extends JpaRepository<FixUpTask, Integer> {
 
+	//Query 10.1 (METIENDO ID DE CUSTOMER)
 	@Query("select distinct c.fixuptasks from Customer c where c.id=?1")
 	Collection<FixUpTask> findByCustomerId(int customerId);
+
+	/*
+	 * //Query 10.1 (METIENDO ID DE USERACCOUNT)
+	 * 
+	 * @Query("select distinct c.fixuptasks from Customer c where c.id=?1")
+	 * Collection<FixUpTask> findByCustomerId(int customerId);
+	 */
+
+	//Query 11.2
+	/*
+	 * static final String qry = "select f from FixUpTask f";
+	 * if(keyword)
+	 */
 
 }
